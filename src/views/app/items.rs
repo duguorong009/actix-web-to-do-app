b4bc544f2cd09e6d37c44906931293bc97e366fa
duguorong <dudurong009@gmail.com>
2023-02-11 22:45:04 +0800

    Complete html+css+js

--- src/views/app/items.rs
+++ src/views/app/items.rs
@@ -3,7 +3,12 @@ use actix_web::HttpResponse;
 use super::content_loader::read_file;
 
 pub async fn items() -> HttpResponse {
-    let html_data = read_file("./templates/main.html");
+    let mut html_data = read_file("./templates/main.html");
+    let javascript_data = read_file("./javascript/main.js");
+    let css_data = read_file("./css/main.css");
+    html_data = html_data.replace("\"{{JAVASCRIPT}}\";", &javascript_data);
+    html_data = html_data.replace("{{CSS}}", &css_data);
+
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html_data)
