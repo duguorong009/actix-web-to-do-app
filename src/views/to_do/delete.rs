fad52d8bb5f6326cbebe9e248c84b97ed0d35425
duguorong <dudurong009@gmail.com>
2023-02-13 15:59:07 +0800

    Update the "delete" endpoint to use DB read/write

--- src/views/to_do/delete.rs
+++ src/views/to_do/delete.rs
@@ -1,23 +1,27 @@
 use actix_web::{web, HttpResponse};
-use serde_json::{Map, Value};
 
-use crate::{
-    json_serialization::to_do_item::ToDoItem, processes::process_input, state::read_file,
-    to_do::to_do_factory,
-};
+use crate::json_serialization::to_do_item::ToDoItem;
+
+use crate::diesel;
+use diesel::prelude::*;
+
+use crate::database::establish_connection;
+use crate::models::item::item::Item;
+use crate::schema::to_do;
 
 use super::utils::return_state;
 
 pub async fn delete(to_do_item: web::Json<ToDoItem>) -> HttpResponse {
-    let state: Map<String, Value> = read_file("./state.json");
-
     let title: String = to_do_item.title.clone();
-    let status: String = to_do_item.status.clone();
 
-    match to_do_factory(&status, &title) {
-        Err(_) => return HttpResponse::BadRequest().json(format!("{} not accepted", status)),
-        Ok(item) => process_input(item, String::from("delete"), &state),
-    }
+    let mut connection = establish_connection();
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(title.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&mut connection)
+        .unwrap();
+
+    let _ = diesel::delete(&items[0]).execute(&mut connection);
 
     HttpResponse::Ok().json(return_state())
 }
