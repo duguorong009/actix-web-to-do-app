8216c92b1881f6f427f99f485c5b60f231f7538f
duguorong <dudurong009@gmail.com>
2023-02-13 22:43:16 +0800

    Add the "create_user" endpoint for user creation

---
+++ src/models/user/new_user.rs
@@ -0,0 +1,29 @@
+extern crate bcrypt;
+
+use bcrypt::{hash, DEFAULT_COST};
+use diesel::Insertable;
+use uuid::Uuid;
+
+use crate::schema::users;
+
+#[derive(Insertable, Clone)]
+#[table_name = "users"]
+pub struct NewUser {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+    pub unique_id: String,
+}
+
+impl NewUser {
+    pub fn new(username: String, email: String, password: String) -> NewUser {
+        let password: String = hash(password.as_str(), DEFAULT_COST).unwrap();
+        let uuid = Uuid::new_v4().to_string();
+        NewUser {
+            username,
+            email,
+            password,
+            unique_id: uuid,
+        }
+    }
+}
--- src/views/mod.rs
+++ src/views/mod.rs
@@ -5,9 +5,11 @@ mod auth;
 mod path;
 mod to_do;
 pub mod token;
+mod users;
 
 pub fn views_factory(app: &mut web::ServiceConfig) {
     auth::auth_factory(app);
     to_do::item_factory(app);
     app::app_factory(app);
+    users::user_factory(app);
 }
