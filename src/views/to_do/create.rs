0639318cd83b8176188fa231ea47b4053bbecaa9
duguorong <dudurong009@gmail.com>
2023-02-13 15:44:58 +0800

    Update the "edit" endpoint to use the DB read/write

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,16 +1,32 @@
-use actix_web::HttpRequest;
-use serde_json::{Map, Value};
+use crate::diesel;
+use actix_web::{web, HttpRequest, Responder};
+use diesel::prelude::*;
 
-use crate::{processes::process_input, state::read_file, to_do};
+use super::utils::return_state;
+use crate::database::establish_connection;
+use crate::models::item::item::Item;
+use crate::models::item::new_item::NewItem;
+use crate::schema::to_do;
 
-pub async fn create(req: HttpRequest) -> String {
-    let state: Map<String, Value> = read_file("./state.json");
-
+pub async fn create(req: HttpRequest) -> impl Responder {
+    // Get the params fron request
     let title: String = req.match_info().get("title").unwrap().to_string();
     let title_reference: String = title.clone();
 
-    let item = to_do::to_do_factory("pending", &title).expect("create ");
-    process_input(item, "create".to_string(), &state);
+    // Make a db call
+    let mut connection = establish_connection();
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(title_reference.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&mut connection)
+        .unwrap();
 
-    format!("{} created", title_reference)
+    if items.len() == 0 {
+        let new_post = NewItem::new(title);
+        let _ = diesel::insert_into(to_do::table)
+            .values(&new_post)
+            .execute(&mut connection);
+    }
+
+    web::Json(return_state())
 }
